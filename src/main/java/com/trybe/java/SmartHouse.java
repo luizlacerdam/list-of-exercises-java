cbcff40ebfee5365f15814bb5e3996289f47ca71
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-03 21:10:51 -0300

    SmartHouse-conectarInternet

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -17,4 +17,18 @@ public class SmartHouse {
         lampadaLigada = false;
     }
 
+    public boolean conectarInternet() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Tentando conectar à internet...");
+            double randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                System.out.println("Conectado à internet!");
+                return true;
+            } else {
+                System.out.println("Falha ao conectar à internet!");
+            }
+        }
+        System.out.println("Conectando à internet...");
+        return true;
+    }
 }
