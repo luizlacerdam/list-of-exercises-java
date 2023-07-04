4b0fa76345d433c60f7d6079e1f8b5dbf1d6cb70
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-04 10:11:37 -0300

    build-done

--- src/main/java/com/trybe/java/House.java
+++ src/main/java/com/trybe/java/House.java
@@ -5,43 +5,57 @@ package com.trybe.java;
  **/
 
 public class House {
-    private String nome;
-    private int idade;
-    private double peso;
-    private double altura;
 
-    public House(String nome, int idade, double peso, double altura) {
-        this.nome = nome;
-        this.idade = idade;
-        this.peso = peso;
-        this.altura = altura;
-    }
+  private String nome;
+  private int idade;
+  private double peso;
+  private double altura;
 
-    public String getNome() {
-        return this.nome;
-    }
+  /**
+   * Construtor da classe House.
+   *
+   * @param nome   O nome da pessoa.
+   * @param idade  A idade da pessoa.
+   * @param peso   O peso da pessoa.
+   * @param altura A altura da pessoa.
+   */
+  public House(String nome, int idade, double peso, double altura) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+  }
 
-    public int getIdade() {
-        return this.idade;
-    }
+  public String getNome() {
+    return this.nome;
+  }
 
-    public double getPeso() {
-        return this.peso;
-    }
+  public int getIdade() {
+    return this.idade;
+  }
 
-    public double getAltura() {
-        return this.altura;
-    }
+  public double getPeso() {
+    return this.peso;
+  }
 
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
+  public double getAltura() {
+    return this.altura;
+  }
 
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
+  public void setPeso(double peso) {
+    this.peso = peso;
+  }
 
-    public double calculaImc() {
-        return this.peso / (this.altura * this.altura);
-    }
+  public void setAltura(double altura) {
+    this.altura = altura;
+  }
+  /**
+   * Calcula o Índice de Massa Corporal (IMC) com base no peso e altura.
+   *
+   * @return O valor do IMC.
+   */
+
+  public double calculaImc() {
+    return this.peso / (this.altura * this.altura);
+  }
 }
--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -7,58 +7,75 @@ import java.util.HashMap;
  **/
 
 public class SmartHouse {
-    public boolean lampadaLigada = false;
-    private House house;
-    public HashMap<String, String> estadosBrasileiros = new HashMap<String, String>();
 
-    public SmartHouse() {
-        this.house = null;
-        estadosBrasileiros.put("SP", "São Paulo");
-        estadosBrasileiros.put("RJ", "Rio de Janeiro");
-        estadosBrasileiros.put("MG", "Minas Gerais");
-        estadosBrasileiros.put("PE", "Pernambuco");
-        estadosBrasileiros.put("SC", "Santa Catarina");
-        estadosBrasileiros.put("AM", "Amazonas");
-    }
+  public boolean lampadaLigada = false;
+  private House house;
+  public HashMap<String, String> estadosBrasileiros = new HashMap<String, String>();
 
-    public House getHouse() {
-        return this.house;
-    }
+  /**
+   * Construtor da classe SmartHouse.
+   */
+  public SmartHouse() {
+    this.house = null;
+    estadosBrasileiros.put("SP", "São Paulo");
+    estadosBrasileiros.put("RJ", "Rio de Janeiro");
+    estadosBrasileiros.put("MG", "Minas Gerais");
+    estadosBrasileiros.put("PE", "Pernambuco");
+    estadosBrasileiros.put("SC", "Santa Catarina");
+    estadosBrasileiros.put("AM", "Amazonas");
+  }
 
-    public void setHouse(House house) {
-        this.house = house;
-    }
+  public House getHouse() {
+    return this.house;
+  }
 
-    public String mostraNomeEstado(String sigla) {
-        return estadosBrasileiros.get(sigla);
-    }
+  public void setHouse(House house) {
+    this.house = house;
+  }
 
-    public void ligarLampada() {
-        System.out.println("Ligando a lâmpada...");
-        lampadaLigada = true;
-    }
+  /**
+   * Obtém o nome do estado brasileiro correspondente à sigla fornecida.
+   *
+   * @param sigla a sigla do estado brasileiro.
+   * @return o nome do estado correspondente à sigla fornecida.
+   */
 
-    public void desligarLampada() {
-        System.out.println("Desligando a lâmpada...");
-        lampadaLigada = false;
-    }
+  public String mostraNomeEstado(String sigla) {
+    return estadosBrasileiros.get(sigla);
+  }
 
-    public boolean isLampadaLigada() {
-        return lampadaLigada;
-    }
+  public void ligarLampada() {
+    System.out.println("Ligando a lâmpada...");
+    lampadaLigada = true;
+  }
 
-    public boolean conectarInternet() {
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Tentando conectar à internet...");
-            double randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                System.out.println("Conectado à internet!");
-                return true;
-            } else {
-                System.out.println("Falha ao conectar à internet!");
-            }
-        }
-        System.out.println("Conectando à internet...");
+  public void desligarLampada() {
+    System.out.println("Desligando a lâmpada...");
+    lampadaLigada = false;
+  }
+
+  public boolean isLampadaLigada() {
+    return lampadaLigada;
+  }
+
+  /**
+   * Conecta-se à internet.
+   *
+   * @return true se a conexão foi bem-sucedida, false caso contrário.
+   */
+
+  public boolean conectarInternet() {
+    for (int i = 0; i < 3; i++) {
+      System.out.println("Tentando conectar à internet...");
+      double randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        System.out.println("Conectado à internet!");
         return true;
+      } else {
+        System.out.println("Falha ao conectar à internet!");
+      }
     }
+    System.out.println("Conectando à internet...");
+    return true;
+  }
 }
